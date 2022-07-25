33821286cd0b2b7e16f25a41a0be54088619932a
LuisErnie <lefr@live.com.mx>
2022-07-25 14:31:56 -0500

    Ps4 controller commands added

---
+++ CustomController.py
@@ -0,0 +1,152 @@
+from pyPS4Controller.controller import Controller
+
+
+class ControllerConfig(Controller):
+
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+
+    # Drive controls
+    def on_x_press(self):
+        ON_Motor = 1
+        print("Pedal to the metal")
+
+    def on_x_release(self):
+        ON_Motor = 0
+        print("Brake!!!")
+    
+    def on_triangle_press(self):
+        pass
+    
+    def on_triangle_release(self):
+        pass
+    
+    def on_circle_press(self):
+        pass
+    
+    def on_circle_release(self):
+        pass
+    
+    def on_square_press(self):
+        pass
+    
+    def on_square_release(self):
+        pass
+
+    def on_up_arrow_press(self):
+        R_MTR = 0
+        L_MTR = 1
+        FWD = 1
+        BWD = 0
+        print("Forward")
+
+    def on_down_arrow_press(self):
+        R_MTR = 0
+        L_MTR = 1
+        FWD = 0
+        BWD = 1
+        print("Backward")
+    
+    def on_up_down_arrow_release(self):
+        print ("No Direction")
+    
+    def on_left_arrow_press(self):
+        print("Left")
+
+    def on_right_arrow_press(self):
+        print("Right")
+
+    def on_left_right_arrow_release(self):
+        print ("No Direction")
+
+    def on_L1_press(self):
+        pass
+
+    def on_L1_release(self):
+        pass
+
+    def on_L2_press(self, value):
+        print("on_L2_press: {}".format(value))
+
+    def  on_L2_release(self):
+        pass
+    
+    def on_R1_press(self):
+        pass
+
+    def on_R1_release(self):
+        pass
+
+    def on_R2_press(self, value):
+        print("on_R2_press: {}".format(value))
+
+    def on_R2_release(self):
+        pass
+
+    def on_L3_up(self, value):
+        print("on_L3_up: {}".format(value))
+
+    def on_L3_down(self, value):
+        print("on_L3_down: {}".format(value))
+
+    def on_L3_left(self, value):
+        print("on_L3_left: {}".format(value))
+
+    def on_L3_right(self, value):
+        print("on_L3_right: {}".format(value))
+
+    def on_L3_y_at_rest(self):
+        """L3 joystick is at rest after the joystick was moved and let go off"""
+        print("on_L3_y_at_rest")
+
+    def on_L3_x_at_rest(self):
+        """L3 joystick is at rest after the joystick was moved and let go off"""
+        print("on_L3_x_at_rest")
+
+    def on_L3_press(self):
+        """L3 joystick is clicked. This event is only detected when connecting without ds4drv"""
+        print("on_L3_press")
+
+    def on_L3_release(self):
+        """L3 joystick is released after the click. This event is only detected when connecting without ds4drv"""
+        print("on_L3_release")
+
+    def on_R3_up(self, value):
+        print("on_R3_up: {}".format(value))
+
+    def on_R3_down(self, value):
+        print("on_R3_down: {}".format(value))
+
+    def on_R3_left(self, value):
+        print("on_R3_left: {}".format(value))
+
+    def on_R3_right(self, value):
+        print("on_R3_right: {}".format(value))
+
+    def on_R3_y_at_rest(self):
+        """R3 joystick is at rest after the joystick was moved and let go off"""
+        print("on_R3_y_at_rest")
+
+    def on_R3_x_at_rest(self):
+        """R3 joystick is at rest after the joystick was moved and let go off"""
+        print("on_R3_x_at_rest")
+
+    def on_R3_press(self):
+        """R3 joystick is clicked. This event is only detected when connecting without ds4drv"""
+        print("on_R3_press")
+
+    def on_R3_release(self):
+        """R3 joystick is released after the click. This event is only detected when connecting without ds4drv"""
+        print("on_R3_release")
+
+    def on_options_press(self):
+        pass
+
+    def on_options_release(self):
+        pass
+
+    def on_share_press(self):
+        pass  # this event is only detected when connecting without ds4drv
+
+    def on_share_release(self):
+        pass  # this event is only detected when connecting without ds4drv
--- omni_bot.py
+++ omni_bot.py
@@ -1,16 +1,104 @@
+#!/usr/bin/env python3
+#---------------------------------------------------------------------------------
+# Remote control of omnidirectional robot using PS4 controller
+#---------------------------------------------------------------------------------
+#
+# Written by Luis Erneto Fernandez
+# Universidad Autónoma de Querétaro, July 2022
+# 
+# This code relys in the qwiic and the ps4drv python libraries for using the Auto
+# Phat from Sparkfun.
+#
+# More information on qwiic is at https://www.sparkfun.com/qwiic
+# sudo pip3 install sparkfun-qwiic
+# sudo pip3 install --upgrade sparkfun-qwiic-scmd
+#
+# More information on ps4drv is at https://github.com/ArturSpirin/pyPS4Controller
+# sudo pip install pyPS4Controller
+#
+#================================================================================
+
+
 from pyPS4Controller.controller import Controller
 
+from CustomController import ControllerConfig
 
-def konami_callback():
-    print("Konami sequence detected!")
+# from __future__ import print_function
+import time
+import sys
+import math
+import qwiic_scmd
+
+# Initialization of the motor driver
+myMotorF = qwiic_scmd.QwiicScmd(0x5D)
+myMotorB = qwiic_scmd.QwiicScmd(0x5E)
+
+R_MTR = 0
+L_MTR = 1
+FWD = 1
+BWD = 0
+
+ON_Motor = 0
 
 
-def my_sequences():
-    return [
-        {"inputs": ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'share', 'options'],
-         "callback": konami_callback}
-    ]
+controller = ControllerConfig(interface="/dev/input/js0", connecting_using_ds4drv=False)
+# you can start listening before controller is paired, as long as you pair it within the timeout window
+controller.listen(timeout=60)
 
 
-controller = Controller(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.listen(on_sequence=my_sequences())
+"""
+def drive():
+	print("Motor Test.")
+	R_MTR = 0
+	L_MTR = 1
+	FWD = 0
+	BWD = 1
+
+	if myMotorF.connected == False and myMotorB.connected == False:
+		print("Motor Driver not connected. Check connections.", \
+			file=sys.stderr)
+		return
+	myMotorF.begin()
+	myMotorB.begin()
+	print("Motors initialized.")
+	time.sleep(.250)
+
+	# Zero Motor Speeds
+	myMotorF.set_drive(0,0,0)
+	myMotorF.set_drive(1,0,0)
+	myMotorB.set_drive(0,0,0)
+	myMotorB.set_drive(1,0,0)
+
+	myMotorF.enable()
+	myMotorB.enable()
+	print("Motors enabled")
+	time.sleep(.250)
+
+
+	while True:
+		speed = 20
+		for speed in range(20,255):
+			print(speed)
+			myMotorF.set_drive(R_MTR,FWD,speed)
+			myMotorF.set_drive(L_MTR,BWD,speed)
+			myMotorB.set_drive(R_MTR,FWD,speed)
+			myMotorB.set_drive(L_MTR,BWD,speed)
+			time.sleep(.05)
+		for speed in range(254,20, -1):
+			print(speed)
+			myMotorF.set_drive(R_MTR,FWD,speed)
+			myMotorF.set_drive(L_MTR,BWD,speed)
+			myMotorB.set_drive(R_MTR,FWD,speed)
+			myMotorB.set_drive(L_MTR,FWD,speed)
+			time.sleep(.05)
+
+if __name__ == '__main__':
+	try:
+		runExample()
+	except (KeyboardInterrupt, SystemExit) as exErr:
+		print("Ending example.")
+		myMotorF.disable()
+		myMotorB.disable()
+		sys.exit(0)
+
+"""
